1a59378d5ab274e6e1f223b4db1587f8c95108f5
Wiktor Kotarski <wiktor313k9@gmail.com>
2025-12-08 14:19:10 +0100

    Day 1 - Part 1 and Part 2 solutions

---
+++ Day1/Code/Day1Part1.py
@@ -0,0 +1,22 @@
+import csv
+
+current_state = 50
+iteration = 0
+data = csv.reader(open('Day1\\Resources\\day1Input.csv', 'r'))
+#data = csv.reader(open('Day1\\Resources\\day1Sample.csv', 'r'))
+
+def change_values(state, value):
+    global iteration
+    if value[0] == 'R':
+        state = (state + int(value[1:])) % 100
+    elif value[0] == 'L':
+        state = (state - int(value[1:])) % 100
+    
+    if state == 0:
+        iteration += 1
+    
+    return state
+
+for value in data:
+    current_state = change_values(current_state, value[0])
+print(f"Final State: {current_state}, Iterations: {iteration}")
